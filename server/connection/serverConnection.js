e3b41e75f2b28a55e7d786ca38d57a6746238bd6
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2025-02-25 23:27:53 +0530

    Client side initiated

--- server/connection/serverConnection.js
+++ server/connection/serverConnection.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 async function startServer(uri,expressInstance,PORT){
     try{
         await mongoose.connect(uri);
-        console.log("Connected to database");
-        console.log("Starting Server");
+        console.log("Connected to database!..Starting Server");
+        console.log("Wait for server to start it may take some time!");
         expressInstance.listen(PORT,()=>{
             console.log('Server started on port',PORT);
             console.log("Go to http://localhost:/"+PORT);
--- server/schema/FactCollectionSchema.js
+++ server/schema/FactCollectionSchema.js
@@ -13,7 +13,7 @@ const FactCollectionSchema=new mongoose.Schema({
 
     category:{
         type:String,
-        required:true
+        // required:true
     },
     
     votesIntresting:{
